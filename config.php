e9e49f75005b54efc1a8576cd7f0c9ec8b5b4094
Joran Dob <joran998@gmail.com>
2017-07-05 16:55:30 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 // VWE URL IMPORTANT: add ..../page/ behind url
-$URL = 'https://voorraadmodule.vwe-advertentiemanager.nl/sa2d51d3141b35a54ecfaa8cab1a5aa66/d09e/stock/vehicles/20/page/';
+$URL = 'https://voorraadmodule.vwe-advertentiemanager.nl/../stock/vehicles/20/page/';
 // Number of car pages
 $PAGES = '5';
 
